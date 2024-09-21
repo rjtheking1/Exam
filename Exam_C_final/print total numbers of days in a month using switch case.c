d2464d99fda892b07199a979069ea7143d4f58f2
Raj Patil <cryptomind1234@gmail.com>
2024-09-21 10:03:56 +0530

    print total numbers of days in a month using switch case.c

--- Exam_C_final/print total numbers of days in a month using switch case.c
+++ Exam_C_final/print total numbers of days in a month using switch case.c
@@ -3,6 +3,55 @@
 
 int main()
 {
-	//wap to print total numbers of days in a month using switch case...
-	
+//wap to print total numbers of days in a month using switch case...
+    int month;
+
+    printf("Enter the month number(1-12): ");
+    scanf("%d", &month);
+
+    switch(month)
+    {
+        case 1: printf("31 days");
+        break;
+        
+		case 2: printf("28/29 days");
+        break;
+        
+        case 3: printf("31 days");
+        break;
+        
+        case 4: printf("30 days");
+        break;
+        
+        case 5: printf("31 days");
+        break;
+        
+        case 6: printf("30 days");
+        break;
+        
+        case 7: printf("31 days");
+        break;
+        
+        case 8: printf("31 days");
+        break;
+        
+		case 9: printf("30 days");
+        break;
+        
+        case 10: printf("31 days");
+        break;
+        
+        case 11: printf("30 days");
+        break;
+        
+        case 12: printf("31 days");
+        break;
+        
+        default: printf("Invalid input! Please enter the month number between 1-12");
+
+    }
+
+    
 }
+	
+
